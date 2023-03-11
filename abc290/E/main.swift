9e5807af6ab9e646f40bcfc7d29f526ba0e9b109
factal <joji0425jp@gmail.com>
2023-03-11 20:39:01 +0900

    abc290 e

--- abc290/E/main.swift
+++ abc290/E/main.swift
@@ -3,6 +3,25 @@ import Foundation
 
 func solve(_ N:Int, _ A:[Int]) {
     var ans = 0
+    var cnt = [Int](repeating: 0, count: N+1)
+
+    for x in A {
+        cnt[x] += 1
+    }
+
+    for i in 0..<((N+1) / 2) {
+        let l = A[i]
+        let r = A[N-1-i]
+        ans += (N - 2*i - cnt[l]) * (i + 1)
+        ans += (N - 2*i - cnt[r]) * (i + 1)
+
+        if l != r {
+            ans -= (i + 1)
+        }
+
+        cnt[l] -= 1
+        cnt[r] -= 1
+    }
 
     print(ans)
 }
