9536b7e55e832864fecfa58cbf7b6b71f4d0448a
factal <joji0425jp@gmail.com>
2023-03-13 20:47:17 +0900

    asdfgh

---
+++ arc158/D/main.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let MOD = 7
+
+func main() {
+    // Failed to predict input format
+}
+
+#if DEBUG
+let caseNumber = 1
+_ = freopen("in_\(caseNumber).txt", "r", stdin)
+#endif
+
+main()
---
+++ arc158/F/main.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+
+func solve(_ N:Int, _ M:Int, _ K:Int, _ A:[Int], _ B:[Int]) {
+    var ans = 0
+
+    print(ans)
+}
+
+func main() {
+    var tokenIndex = 0, tokenBuffer = [String]()
+    func readString() -> String {
+        if tokenIndex >= tokenBuffer.count {
+            tokenIndex = 0
+            tokenBuffer = readLine()!.split(separator: " ").map { String($0) }
+        }
+        defer { tokenIndex += 1 }
+        return tokenBuffer[tokenIndex]
+    }
+    func readInt() -> Int { Int(readString())! }
+    func readDouble() -> Double { Double(readString())! }
+    let N = readInt()
+    let M = readInt()
+    let K = readInt()
+    var A = [Int](repeating: 0, count: N)
+    for i in 0..<N {
+        A[i] = readInt()
+    }
+    var B = [Int](repeating: 0, count: N)
+    for i in 0..<N {
+        B[i] = readInt()
+    }
+    _ = solve(N, M, K, A, B)
+}
+
+#if DEBUG
+let caseNumber = 1
+_ = freopen("in_\(caseNumber).txt", "r", stdin)
+#endif
+
+main()
